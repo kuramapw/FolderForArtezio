af06cd09ce9d1e60c521d9fab9e785c79477b2a2
kuramapw <58393029+kuramapw@users.noreply.github.com>
2020-02-26 03:56:37 +0200

    Create 2.sql

---
+++ Lesson9/2.sql
@@ -0,0 +1,13 @@
+create table if not exists persons (
+    name varchar(30) not null,
+    surname varchar(30) not null,
+    rang varchar(30) not null,
+    cash float not null
+);
+insert into persons(name, surname, rang, cash) values ('Naruto','Uzumaki','Hokage',99999999);
+insert into persons(name, surname, rang, cash) values ('Shikamaru','Nara','The Right Hand Hokage',999999);
+insert into persons(name, surname, rang, cash) values ('Sasuke','Uchiha','Shadow Hokage',0);
+insert into persons(name, surname, rang, cash) values ('KakaShi','Hatake','Hokage',15000);
+
+select * from persons where cash<30000;
+select * from persons where rang='Hokage' and cash<30000;
